9b048eeacbf967bb0cca3062dd99dffe220c054b
victoryakovenko1337 <viktor.yakovenko.228@gmail.com>
2023-11-09 14:05:59 +0200

    [main] Implemented Character Movement
    Added Simple Input System from Unity Asset Store

---
+++ Assets/CodeBase/CameraLogic/CameraFollow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _followingObject;
+    [SerializeField] private float _rotationAngleX;
+    [SerializeField] private float _distance;
+    [SerializeField] private float _offsetY;
+
+    private void LateUpdate() 
+    {
+        if (_followingObject == null)
+            return;
+
+        var rotation = Quaternion.Euler(_rotationAngleX, 0, 0);
+
+        var followingPosition = _followingObject.position;
+        followingPosition.y += _offsetY;
+
+        var position = rotation * new Vector3(0, 0, -_distance) + followingPosition;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    public void Follow(GameObject following)
+    {
+        _followingObject = following.transform;
+    }
+}
---
+++ Assets/CodeBase/Hero/HeroMove.cs
@@ -0,0 +1,47 @@
+using CodeBase.Infrastructure;
+using CodeBase.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class HeroMove : MonoBehaviour
+    {
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private int _movementSpeed;
+
+        private IInputService _inputService;
+        private Camera _camera;
+
+        private void Awake() 
+        {
+            _inputService = Game.InputService;    
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+
+            CameraFollow();
+        }
+
+        private void Update() 
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0;
+                movementVector.Normalize();
+
+                transform.forward = movementVector;
+            }
+
+            movementVector += Physics.gravity;
+
+            _characterController.Move(movementVector * _movementSpeed * Time.deltaTime);
+        }
+
+        private void CameraFollow() => _camera.GetComponent<CameraFollow>().Follow(gameObject);
+    }
+}
