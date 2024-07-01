266586b0ef62109f393760f84ae08f7be4da0777
Viktor <viktor.yakovenko.228@gmail.com>
2024-07-01 14:01:00 +0300

    [main] Changed LootPiece prefab

--- Assets/CodeBase/Enemy/LootPiece.cs
+++ Assets/CodeBase/Enemy/LootPiece.cs
@@ -1,10 +1,17 @@
-﻿using CodeBase.Data;
+﻿using System.Collections;
+using CodeBase.Data;
+using TMPro;
 using UnityEngine;
 
 namespace CodeBase.Enemy
 {
     public class LootPiece : MonoBehaviour
     {
+        public GameObject Skull;
+        public GameObject PickupFxPrefab;
+        public TextMeshPro LootText;
+        public GameObject PickupPopup;
+
         private Loot _loot;
         private bool _picked;
         private WorldData _worldData;
@@ -29,7 +36,33 @@ namespace CodeBase.Enemy
 
             _picked = true;
 
+            UpdateWorldData();
+            HideSkull();
+            PlayPickupFx();
+            ShowText();
+            StartCoroutine(StartDestroyTimer());
+        }
+
+        private void UpdateWorldData() =>
             _worldData.LootData.Collect(_loot);
+
+        private void HideSkull() =>
+            Skull.SetActive(false);
+
+        private void PlayPickupFx() =>
+            Instantiate(PickupFxPrefab, transform.position, Quaternion.identity);
+
+        private void ShowText()
+        {
+            LootText.text = $"{_loot.Value}";
+            PickupPopup.SetActive(true);
+        }
+
+        private IEnumerator StartDestroyTimer()
+        {
+            yield return new WaitForSeconds(1.5f);
+
+            Destroy(gameObject);
         }
     }
 }
