7dc82625882832014aa0623e7ea6d75883598d8a
Viktor <viktor.yakovenko.228@gmail.com>
2024-08-17 16:22:16 +0300

    Some minor changes

--- Assets/CodeBase/Enemy/EnemyAnimator.cs
+++ Assets/CodeBase/Enemy/EnemyAnimator.cs
@@ -1,6 +1,4 @@
-﻿using CodeBase.Logic;
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CodeBase.Enemy
 {
@@ -12,17 +10,7 @@ namespace CodeBase.Enemy
         private static readonly int Hit = Animator.StringToHash("Hit");
         private static readonly int Die = Animator.StringToHash("Die");
 
-        private readonly int _idleStateHash = Animator.StringToHash("idle");
-        private readonly int _attackStateHash = Animator.StringToHash("attack01");
-        private readonly int _walkingStateHash = Animator.StringToHash("Move");
-        private readonly int _deathStateHash = Animator.StringToHash("die");
-
         private Animator _animator;
-
-        public event Action<AnimatorState> StateEntered;
-        public event Action<AnimatorState> StateExited;
-
-        public AnimatorState State { get; private set; }
 
         private void Awake()
         {
@@ -42,31 +30,5 @@ namespace CodeBase.Enemy
         public void StopMoving() => _animator.SetBool(IsMoving, false);
 
         public void PlayAttack() => _animator.SetTrigger(Attack);
-
-        public void EnteredState(int stateHash)
-        {
-            State = StateFor(stateHash);
-            StateEntered?.Invoke(State);
-        }
-
-        public void ExitedState(int stateHash) => StateExited?.Invoke(StateFor(stateHash));
-
-        private AnimatorState StateFor(int stateHash)
-        {
-            AnimatorState state;
-
-            if (stateHash == _idleStateHash)
-                state = AnimatorState.Idle;
-            else if (stateHash == _attackStateHash)
-                state = AnimatorState.Attack;
-            else if (stateHash == _walkingStateHash)
-                state = AnimatorState.Walking;
-            else if (stateHash == _deathStateHash)
-                state = AnimatorState.Died;
-            else
-                state = AnimatorState.Unknown;
-
-            return state;
-        }
     }
 }
